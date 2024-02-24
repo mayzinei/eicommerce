31765bc7bc892f4c0758afa9feaab00d6d7d6046
Than Htike Zaw <thanhtikezaw919@gmail.com>
2024-02-25 01:26:56 +0630

    crud for category

---
+++ app/admin/components/category/modals/updateCategory.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const UpdateModal = ({ titleRef, error, setError, showUpdateModal, setUpdateModal, updateCategory, category }) => {
+  const [updatedTitle, setUpdatedTitle] = useState(category.title);
+
+  const handleTitleChange = (event) => {
+    setUpdatedTitle(event.target.value);
+  };
+
+  return (
+    <div className="absolute z-10 inset-0 overflow-y-auto flex items-center justify-center">
+      <div className="inset-0 bg-black opacity-50"></div>
+      <div className="bg-white rounded-lg p-8 w-full max-w-lg mx-5 shadow-xl">
+        <h2 className="text-xl font-semibold mb-4">Update Category</h2>
+        <form onSubmit={(event) => updateCategory(event)}>
+          <div className="mb-4">
+            <label htmlFor="categoryName" className="block text-sm font-medium text-gray-700">Category Name</label>
+            <input
+              ref={titleRef}
+              onChange={handleTitleChange}
+              value={updatedTitle}
+              type="text"
+              id="categoryName"
+              name="categoryName"
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring focus:border-blue-500"
+            />
+            {error ?
+              <span className='text-red-500 text-sm'>Enter a title</span>
+              : ''
+            }
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="button"
+              className="px-4 py-2 mr-2 bg-gray-300 text-gray-800 font-semibold rounded-lg shadow-md"
+              onClick={() => setUpdateModal(!showUpdateModal)}
+            >
+              Cancel
+            </button>
+            <button
+              onClick={(event) => updateCategory(event)}
+              type="submit"
+              className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateModal;
